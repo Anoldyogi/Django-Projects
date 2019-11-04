9028940200bd643ebdf9b4e886cb12fd0ba7336e
Yogesh <dhami.yogesh@gmail.com>
2019-11-04 14:13:18 +0530

    links and database fetch

--- employeeProject/newsApp/views.py
+++ employeeProject/newsApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from newsApp.models import employee
+
 
 # Create your views here.
+def index(request):
+	return render(request,'empApp/index.html')
+def empview(request):
+	emp_list=employee.objects.all()
+	my_dict={'emp_list':emp_list}
+	return render(request,'empApp/emp.html',context=my_dict)
