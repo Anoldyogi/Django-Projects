5caa1c84676bf08fe51df42c692868c7c22372cd
Yogesh <dhami.yogesh@gmail.com>
2019-11-04 08:29:13 +0530

    Employee

---
+++ employeeProject/newsApp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class employee(models.Model):
+	ename=models.CharField(max_length=30)
+	eno=models.IntegerField()
+	esal=models.FloatField()
+	eaddress=models.CharField(max_length=40)
