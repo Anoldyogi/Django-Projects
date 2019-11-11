a631179041419f1e2a4aa7b4078fe195992b22dd
Yogesh <dhami.yogesh@gmail.com>
2019-11-11 08:59:20 +0530

    Implicit Validation

--- feedbackProject/feedbackApp/forms.py
+++ feedbackProject/feedbackApp/forms.py
@@ -1,10 +1,16 @@
 from django import forms
-class feedbackForm(forms.Form):
-	name=forms.CharField()
-	email=forms.EmailField()
-	feedback=forms.CharField(widget=forms.Textarea)
+from django.core import validators
 
-	def clean_name(self):
+def start_with(value):
+	if value[0].lower()!='d':
+		raise forms.ValidationError("Name shoudl start with d")
+
+class feedbackForm(forms.Form):
+	name=forms.CharField(validators=[start_with])
+	email=forms.EmailField()
+	feedback=forms.CharField(widget=forms.Textarea,validators=[validators.MaxLengthValidator(40),validators.MinLengthValidator(10)])
+
+	'''def clean_name(self):
 		inputname=self.cleaned_data['name']
 		if len(inputname)<4:
 			raise forms.ValidationError("length is less then 4")
@@ -13,4 +19,4 @@ class feedbackForm(forms.Form):
 	def clean_email(self):
 		inputemail=self.cleaned_data['email']
 		print('validating email')
-		return inputemail	
+		return inputemail	'''
